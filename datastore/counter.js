1a6a9a839c49e26f98fdf40daf7eb829bf897baa
Manuela Gomez <mmanu.gomez@gmail.com>
2019-09-24 23:29:09 -0400

    [wip] Complete getNextUniqueId

--- datastore/counter.js
+++ datastore/counter.js
@@ -12,8 +12,27 @@ var counter = 0;
 // https://www.google.com/search?q=what+is+a+zero+padded+number%3F
 
 const zeroPaddedNumber = (num) => {
+  // sprintf returns a formatted string
+  // The placeholders in the format string are marked by % followed by
+  // various elements, which in this case is first a "0":
+  // An optional padding specifier that says what character to use for padding
+  // Then, 5: says how many characters the result should have.
+  // A type specifier, d (or i), which yields an integer as a signed decimal number
   return sprintf('%05d', num);
 };
+
+// Node.js core modules, as well as most of the community-published ones,
+// follow a pattern whereby the first argument to any callback handler is an optional error object.
+// If there is no error, the argument will be null or undefined.
+// A typical callback handler could therefore perform error handling as follows:
+//
+// function callback(err, results) {
+//   // usually we'll check for the error before handling results
+//   if(err) {
+//   // handle error somehow and return
+//   }
+//   // no error, perform standard callback handling
+//   }
 
 const readCounter = (callback) => {
   fs.readFile(exports.counterFile, (err, fileData) => {
@@ -38,8 +57,16 @@ const writeCounter = (count, callback) => {
 
 // Public API - Fix this function //////////////////////////////////////////////
 
+// the counter is set at 0 at line 5
+// so it resets to 0 every time this file reloads
 exports.getNextUniqueId = () => {
-  counter = counter + 1;
+  // we have to read the counterfile and find the next available counter
+  readCounter((error, previousCounter) => {
+    counter = previousCounter + 1;
+    writeCounter(counter, (error, nextUniqueID) => {
+      console.log(nextUniqueID);
+    });
+  });
   return zeroPaddedNumber(counter);
 };
 
